7d35ec5c676021acb6765c0e2b3625cc6b6a974a
denispetko <den.petko@rambler.ru>
2023-01-11 12:24:47 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -4,63 +4,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*transport.Cars[] cars = new transport.Cars[5];
-        cars[0] = new transport.Cars();
-        cars[0].brand = "Lada";
-        cars[0].model = "Granta";
-        cars[0].year = 2015;
-        cars[0].country = "Россия";
-        cars[0].color = "жёлтый";
-        cars[0].engineVolume = 1.7;
-
-        cars[1] = new transport.Cars();
-        cars[1].brand = "Audi";
-        cars[1].model = "50 L TDI quattro";
-        cars[1].year = 2020;
-        cars[1].country = "Германия";
-        cars[1].color = "черный";
-        cars[1].engineVolume = 3.0;
-
-        cars[2] = new transport.Cars();
-        cars[2].brand = "BMW";
-        cars[2].model = "Z8";
-        cars[2].year = 2021;
-        cars[2].country = "Германия";
-        cars[2].color = "черный";
-        cars[2].engineVolume = 3.0;
-
-        cars[3] = new transport.Cars();
-        cars[3].brand = "Kia";
-        cars[3].model = "Sportage";
-        cars[3].year = 2018;
-        cars[3].country = "Южная Корея";
-        cars[3].color = "красный";
-        cars[3].engineVolume = 2.4;
-
-        cars[4] = new transport.Cars();
-        cars[4].brand = "Hyundai";
-        cars[4].model = "Avante";
-        cars[4].year = 2016;
-        cars[4].country = "Южная Корея";
-        cars[4].color = "оранжевый";
-        cars[4].engineVolume = 1.7;*/
 
         Car[] cars = new Car[5];
-        cars[0] = new Car("Lada", "Granta", 2015, "Россия", "жёлтый", 0, "механическая", "седан", "Р985УХ197", 4, true, null);
-        cars[1] = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0, "роботизированная", "седан", "Р985УХ197", 4, true, true);
-        cars[2] = new Car("BMW", null, 2021, "Германия", "черный", 3.0, "роботизированная", "купэ", "Р985УХ197", 2, true, false);
-        cars[3] = new Car("Kia", "Sportage", 2018, null, "красный", 2.4, "автоматическая", "кроссовер", "Р985УХ197", 5, true);
-        cars[4] = new Car("Hyundai", "Avante", -3, "Южная Корея", "", 1.6, "автоматическая", "седан", "Р985УХ197", 5, true);
+        cars[0] = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия", "механическая", "седан", "Р985УХ197", 4, true, new Car.Key(true, true));
+        cars[1] = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия", "роботизированная", "седан", "Р985УХ197", 4, true, null);
+        cars[2] = new Car("BMW", null, 3.0, "черный", 2021, "Германия", "роботизированная", "купэ", "Р985УХ197", 2, true, null);
+        cars[3] = new Car("Kia", "Sportage", 2.4, "красный", 2018, "Южная Корея", "автоматическая", "кроссовер", "Р985УХ197", 5, false, null);
+        cars[4] = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея", "автоматическая", "седан", "Р985УХ197", 5, false, null);
 
-        //System.out.println(Arrays.toString(cars));
-        //или так
-        cars[1].changeTyre(1);
-        cars[0].setRegistrationNumber("А777АА777");
-        cars[0].setRegistrationNumber("А777АА7М7");
+        cars[0].changeTyre(4);
+        cars[0].setRegistrationNumber("A777АА777");
         System.out.println(cars[0].isRegistrationNumber());
         for (Car car : cars) {
             System.out.println(car);
         }
+
+        cars[0].printInfo();
+        cars[1].printInfo();
 
     }
 }
--- src/transport/Car.java
+++ src/transport/Car.java
@@ -8,87 +8,91 @@ public class Car {
     private final int year;
     private final String country;
     private String transmission;
-    private final String typeCarcase;
+    private final String typeCarCase;
     private String registrationNumber;
     private final int seats;
     private boolean summerTyre;
     private Key key;
 
+    private static final String DEFAULT_VALUE = "default";
+    private static final double DEFAULT_VALUE_ENGINE = 1.5;
+    private static final String DEFAULT_COLOR = "белый";
+    private static final int DEFAULT_YEAR = 2000;
+    private static final int DEFAULT_SEATS = 4;
+
+    public Car(String brand, String model, double engineVolume,
+               String color, int year, String country, String transmission,
+               String typeCarCase, String registrationNumber, int seats,
+               boolean summerTyre, Key key) {
+        if (brand == null || brand.isEmpty()) {
+            this.brand = DEFAULT_VALUE;
+        } else {
+            this.brand = brand;
+        }
+
+        if (model == null || model.isEmpty()) {
+            this.model = DEFAULT_VALUE;
+        } else {
+            this.model = model;
+        }
+
+        if (year <= 0) {
+            this.year = DEFAULT_YEAR;
+        } else {
+            this.year = year;
+        }
+
+        if (country == null || country.isEmpty()) {
+            this.country = DEFAULT_VALUE;
+        } else {
+            this.country = country;
+        }
+
+        if (typeCarCase == null || typeCarCase.isEmpty()) {
+            this.typeCarCase = DEFAULT_VALUE;
+        } else {
+            this.typeCarCase = typeCarCase;
+        }
+
+        if (seats <= 0) {
+            this.seats = DEFAULT_SEATS;
+        } else {
+            this.seats = seats;
+        }
+
+        setEngineVolume(engineVolume);
+        setColor(color);
+        setEngineVolume(engineVolume);
+        setTransmission(transmission);
+        setRegistrationNumber(registrationNumber);
+        setSummerTyre(summerTyre);
+        setKey(key);
+    }
+
     public static class Key {
         private final boolean remoteStartEngine;
-        private final boolean nonKeyAccses;
+        private final boolean nonKeyAccess;
 
-        public Key(boolean remoteStartEngine, boolean nonKeyAccses) {
+        public Key(boolean remoteStartEngine, boolean nonKeyAccsess) {
             this.remoteStartEngine = remoteStartEngine;
-            this.nonKeyAccses = nonKeyAccses;
+            this.nonKeyAccess = nonKeyAccsess;
         }
 
         public boolean isRemoteStartEngine() {
             return remoteStartEngine;
         }
 
-        public boolean isNonKeyAccses() {
-            return nonKeyAccses;
-        }
-    }
-
-
-
-    public Car(String brand, String model, int year, String country, String color, double engineVolume, String transmission, String typeCarcase, String registrationNumber, int seats, boolean summerTyre, Key key) {
-        if (brand == null || brand.equals("")) {
-            brand = "default";
-        }
-        this.brand = brand;
-
-        if (model == null || model.equals("")) {
-            model = "default";
-        }
-        this.model = model;
-
-        if (year <= 0) {
-            year = 2000;
-        }
-        this.year = year;
-
-        if (country == null || country.equals("")) {
-            country = "default";
-        }
-        this.country = country;
-
-        if (color == null || color.equals("")) {
-            color = "белый";
-        }
-        this.color = color;
-
-        if (engineVolume <= 0) {
-            engineVolume = 1.5;
-        }
-        this.engineVolume = engineVolume;
-
-        if (transmission == null || transmission.equals("")) {
-            transmission = "автоматическая";
-        }
-        this.transmission = transmission;
-
-        if (typeCarcase == null || typeCarcase.equals("")) {
-            typeCarcase = "седан";
-        }
-        this.typeCarcase = typeCarcase;
-
-        if (registrationNumber == null || registrationNumber.equals("")) {
-            registrationNumber = "х000хх000";
-        }
-        this.registrationNumber = registrationNumber;
-
-        if (seats == 0) {
-            seats = 4;
-        }
-        this.seats = seats;
-        this.summerTyre = summerTyre;
-        if (key == null) {
-            this.key = new Key(false, false);
+        public boolean isNonKeyAccess() {
+            return nonKeyAccess;
         }
 
+        @Override
+        public String toString() {
+            return "Key{" +
+                    "remoteStartEngine=" + remoteStartEngine +
+                    ", nonKeyAccess=" + nonKeyAccess +
+                    '}';
+        }
     }
 
     public String getBrand() {
@@ -103,12 +107,16 @@ public class Car {
         return year;
     }
 
+    public String getColor() {
+        return color;
+    }
+
     public String getCountry() {
         return country;
     }
 
     public String getTypeCarcase() {
-        return typeCarcase;
+        return typeCarCase;
     }
 
     public int getSeats() {
@@ -119,16 +127,20 @@ public class Car {
         return engineVolume;
     }
 
-    public void setEngineVolume(double engineVolume) {
-        this.engineVolume = engineVolume;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
     public void setColor(String color) {
-        this.color = color;
+        if (color == null || color.isEmpty()) {
+            this.color = DEFAULT_COLOR;
+        } else {
+            this.color = model;
+        }
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        if (engineVolume <= 0) {
+            this.engineVolume = DEFAULT_VALUE_ENGINE;
+        } else {
+            this.engineVolume = engineVolume;
+        }
     }
 
     public String getTransmission() {
@@ -136,7 +148,11 @@ public class Car {
     }
 
     public void setTransmission(String transmission) {
-        this.transmission = transmission;
+        if (transmission == null || transmission.isEmpty()) {
+            this.transmission = DEFAULT_VALUE;
+        } else {
+            this.transmission = transmission;
+        }
     }
 
     public String getRegistrationNumber() {
@@ -144,7 +160,11 @@ public class Car {
     }
 
     public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
+        if (registrationNumber == null || registrationNumber.isEmpty()) {
+            this.registrationNumber = DEFAULT_VALUE;
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
     }
 
     public void setSummerTyre(boolean summerTyre) {
@@ -152,8 +172,14 @@ public class Car {
     }
 
     public void changeTyre(int month) {
-        if (month == 12 || (month > 0 && month <3)) {
-            summerTyre = !summerTyre;
+        setSummerTyre(month > 2 && month < 12);
+    }
+
+    public void setKey(Key key) {
+        if (key == null) {
+            this.key = new Key(false, false);
+        } else {
+            this.key = key;
         }
     }
 
@@ -162,36 +188,41 @@ public class Car {
             return false;
         }
         char[] chars = registrationNumber.toCharArray();
-        if (!Character.isAlphabetic(chars[0]) || Character.isAlphabetic(chars[4]) || Character.isAlphabetic(chars[5])) {
+        if (!Character.isAlphabetic(chars[0]) || !Character.isAlphabetic(chars[4]) || !Character.isAlphabetic(chars[5])) {
             return false;
         }
         if (!Character.isDigit(chars[1]) || !Character.isDigit(chars[2]) || !Character.isDigit(chars[3]) || !Character.isDigit(chars[6]) || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])) {
             return false;
         }
         return true;
+
     }
 
-
-
-
-
-
+        public void printInfo() {
+            System.out.println("brand='" + brand + '\'' +
+                    ", model='" + getModel() + '\'' +
+                    ", engineVolume=" + getEngineVolume() +
+                    ", color='" + getColor() + '\'' +
+                    ", year=" + getYear() +
+                    ", country='" + getCountry() + '\'');
+        }
 
     @Override
-
     public String toString() {
-        return "transport.Cars{" +
+        return "Car{" +
                 "brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 ", engineVolume=" + engineVolume +
                 ", color='" + color + '\'' +
                 ", year=" + year +
                 ", country='" + country + '\'' +
-                "transmission='" + transmission + '\'' +
-                ", typeCarcase='" + typeCarcase + '\'' +
-                "registrationNumber='" + registrationNumber + '\'' +
-                ", seats='" + seats + '\'' +
-                ", summerTyre='" + summerTyre + '\'' +
+                ", transmission='" + transmission + '\'' +
+                ", typeCarCase='" + typeCarCase + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", seats=" + seats +
+                ", summerTyre=" + summerTyre +
+                ", key=" + key +
                 '}';
     }
+
 }
